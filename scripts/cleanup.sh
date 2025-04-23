4410c0463b01e6cfc94aa1babc11e8e5d396f781
aae2079 <aae2079@nyu.edu>
2025-04-22 20:27:48 -0400

    Cleaned up hanging .so and .o files

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,5 +4,8 @@ rm -rf $PROJECT_DIR/bin
 rm -rf $PROJECT_DIR/lib
 rm -rf $PROJECT_DIR/external
 rm -rf $PROJECT_DIR/obj
+rm $PROJECT_DIR/src/libs/reverb-time/*.o
+rm $PROJECT_DIR/src/libs/reverb-time/*.a
+rm $PROJECT_DIR/src/libs/reverb-time/*.so
 
 echo "Clean up done"
