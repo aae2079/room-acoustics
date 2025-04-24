719ca6b570d2e5a364c917ccc510bc60687be1fe
aae2079 <aae2079@nyu.edu>
2025-04-24 19:10:58 -0400

    Changed makefiles so libs are pointing to the correct path and made edits to setEnv to be able to run anywhere in repo

--- scripts/setEnv.sh
+++ scripts/setEnv.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Resolve the path to this script, even if sourced from elsewhere
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export PROJECT_DIR
+if [ -z "PROJECT_DIR" ]; then
+    PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    export PROJECT_DIR
+fi
 
 export LD_LIBRARY_PATH="$PROJECT_DIR/lib:$LD_LIBRARY_PATH"
 
