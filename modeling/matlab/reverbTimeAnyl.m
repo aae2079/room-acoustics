fc86ec434dbce828fe043891028399cccc069084
aae2079 <aae2079@nyu.edu>
2025-04-24 20:01:59 -0400

    Finalized RT Alg migration to matlab

--- modeling/matlab/reverbTimeAnyl.m
+++ modeling/matlab/reverbTimeAnyl.m
@@ -14,7 +14,8 @@
     reverbTime: a struct containing the reverb time values for 60 dB, 30 dB, 20 dB, and 10 dB}
 %}
 
-function reverbTime = reverbTimeCalc(audioData, fs, window_size)
+function reverbTime = reverbTimeAnyl(audioData, fs, window_size,gdb)
+    time = 0:1/fs:(length(audioData)-1)/fs;
     % Initialize return structure
     reverbTime = struct('RT60', NaN, 'RT30', NaN, 'RT20', NaN, 'RT10', NaN);
 
@@ -22,8 +23,21 @@ function reverbTime = reverbTimeCalc(audioData, fs, window_size)
     hilbertTransformedData = hilbert(audioData);
     envelope = abs(hilbertTransformedData);
 
+    if(gdb)
+        % Debugging: Plot the envelope
+        figure(2);
+        plot(envelope);
+        hold on;
+        plot(audioData);
+        hold off;
+        title('Envelope of the signal');
+        xlabel('Sample Number');
+        ylabel('Amplitude');
+    end
+
     % Moving Average
     envelope = movingAverage(envelope, window_size);
+
 
     % Debugging: Convert envelope to dB
     debug = 20 * log10(envelope);
@@ -46,10 +60,10 @@ function reverbTime = reverbTimeCalc(audioData, fs, window_size)
     integralSize = ii;
 
     % Schroeder Integration
-    xSch = schroederIntegrral(flip(envelope(1:integralSize))^2);
+    xSch = schroederIntegrral(flip(envelope(1:integralSize)).^2);
 
     % Find the slope of the curve
-    f = polyfit((1:integralSize), xSch, 1);
+    f = polyfit(time(1:integralSize), xSch, 1);
     slope = f(1);
     b = f(2);
 
@@ -58,6 +72,31 @@ function reverbTime = reverbTimeCalc(audioData, fs, window_size)
     reverbTime.RT30 = -30 / slope;
     reverbTime.RT20 = -20 / slope;
     reverbTime.RT10 = -10 / slope;
+
+    if(gdb)
+        % Debugging: Plot the Schroeder curve
+        figure(16);
+        plot(time,20*log10(audioData));
+        title('Revertb Time Analysis');
+        xlabel('Time (s)');
+        ylabel('Power Level (dB)');
+        hold on;
+        plot(time,20*log10(envelope));
+        plot(time(1:integralSize),xSch(1:integralSize),"LineWidth",2);
+        %plot reverb horizontal lines
+        y = [-60,-30, -20, -10];
+        for i = 1:length(y)
+            line([0 time(end)], [y(i) y(i)], 'Color', 'k', 'LineStyle', '--');
+            text(time(end), y(i), sprintf('RT%d: %.2f', 60 - (i-1)*30, y(i)), 'VerticalAlignment', 'bottom');
+        end
+        %line
+        x = 1:integralSize;
+        y = f(1) * time + f(2);
+        plot(time(1:integralSize), y(1:integralSize), 'r--', 'LineWidth', 2);
+        legend('Signal in dB', 'Envelope', 'Schroeder Curve', 'Fitted Line','Location','Southeast');
+        hold off;
+
+    end
 end
 
 function y = movingAverage(x,k)
